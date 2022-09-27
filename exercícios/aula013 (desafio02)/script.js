8e2062a3d49a432a9886a59f773a74ffd114061d
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-09-27 19:07:56 -0300

    ud aula013 (verificador de idade)

--- exercícios/aula013 (desafio02)/script.js
+++ exercícios/aula013 (desafio02)/script.js
@@ -17,26 +17,36 @@ function verificar() {
             gender = 'um homem'
             if (idade >= 0 && idade <= 10) {
                 //Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
             } else if (idade <= 21) {
                 //Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
             } else {
                 //Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
             }
         } else if (fsex[1].checked) {
             gender = 'uma mulher'
             if (idade >= 0 && idade <= 10) {
                 //Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
             } else if (idade <= 21) {
                 //Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
             } else if (idade < 50) {
                 //Adulta
+                img.setAttribute('src', 'foto-adulto-f.png')
             } else {
                 //Idosa
+                img.setAttribute('src', 'foto-idoso-f.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gender} com ${idade} anos.`
+        res.appendChild(img)
+        //adicionar elemento img 
     }
 }
