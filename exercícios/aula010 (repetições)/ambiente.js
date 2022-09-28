94fda626a7104282db4a79b2e581a12452d56380
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-09-28 15:40:40 -0300

    b

--- exercícios/aula010 (repetições)/ambiente.js
+++ exercícios/aula010 (repetições)/ambiente.js
@@ -1,7 +1,7 @@
 /* var c = 1
 while (c <= 6) {
     console.log(`Passo ${c}`)
-    c++
+    c++ -> c = c + 1
 }
 */
 
@@ -9,5 +9,11 @@ var c = 1
 do {
     console.log(`Passo ${c}`)
     c++
-} while (c <=5)
+} while (c <= 5)
+
+
+
+
+
+
 
