ded22a0c884b8d6faf36a465a957116f491b1b49
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-10-03 17:19:03 -0300

    ud script.js tela de login

--- desafios/teste002/script.js
+++ desafios/teste002/script.js
@@ -1,3 +1,12 @@
-function mostrar() {
-    
+const senha = document.getElementById('senha');
+const olho = document.getElementById('olho');
+
+function showhide() {
+    if (senha.type === 'senha') {
+        senha.setAttribute('type','text');
+        olho.classList.add('hide')
+    } else {
+        senha.setAttribute('type','senha');
+        olho.classList.remove('hide')
+    }
 }
