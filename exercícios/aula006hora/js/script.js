3fc8352aa66e7e5edca4f2b118e6ccdb64d9f041
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-10-06 19:54:04 -0300

    aula006 string

---
+++ exercícios/aula006hora/js/script.js
@@ -0,0 +1,13 @@
+var nome = 'Hugo';
+var sobrenome = 'Coutinho';
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+document.write("Olá, tudo bem? Esse texto está sendo digitado apenas por JS. <br> <br> Escrito por" + " " + nome);
+
+console.log("Este número: " + "432")
