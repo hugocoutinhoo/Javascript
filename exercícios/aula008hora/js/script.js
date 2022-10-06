8c8c597125f175b3eb7d88fed848065057e023de
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-10-06 20:12:07 -0300

    aula008 undefined e null

---
+++ exercícios/aula008hora/js/script.js
@@ -0,0 +1,12 @@
+// Hoisting - içamento (variáveis devem estar no topo do código)
+console.log(sobrenome);
+
+var nome = null;
+var sobrenome = 'Coutinho';
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = 'Hugo Luiz';
+
+console.log(nome);
