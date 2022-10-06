85c594a6cb0f023bb10a08c26dcecf06e4507c3a
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-10-06 17:10:04 -0300

    declarando variáveis

---
+++ exercícios/aula004hora/js/script.js
@@ -0,0 +1,33 @@
+var teste = 1;
+
+console.log(teste);
+
+teste = 'Mateus';
+
+console.log(teste);
+
+var $nome = 'ASD';
+var _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'Hugo' // camelCase
+
+console.log(meuPrimeiroNome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+meuNome = 'Hugo Luiz';
+
+console.log(meuNome)
