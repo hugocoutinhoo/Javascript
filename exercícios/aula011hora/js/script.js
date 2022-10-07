73fb5f035c45c8cdb46f78c8b456379b54265b30
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-10-07 11:30:57 -0300

    aula011 if

---
+++ exercícios/aula011hora/js/script.js
@@ -0,0 +1,22 @@
+var idade = 19;
+var idadeMinima = 18;
+
+if(idade >= idadeMinima) {
+    console.log("Você pode tirar sua habilitação.")
+} else {
+    console.log("Você não tem idade suficiente para tirar sua habilitação.")
+}
+
+
+//O código segue o fluxo de execução normalmente.
+console.log("antes do 'if'");
+
+if(true) {
+   console.log("testando o 'if'");
+}
+/*if(false) {
+    console.log("testando o 'if'") 
+ }*/
+
+ console.log("Deposi do 'if'");
+ 
