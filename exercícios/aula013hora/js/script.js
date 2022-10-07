e7a25de5eb83f928fb2747c10d5d5c6e327411a0
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-10-07 17:33:45 -0300

    aula013 operadores condicionais (else e else if)

---
+++ exercícios/aula013hora/js/script.js
@@ -0,0 +1,18 @@
+var nome = "Hugo"
+
+if(nome == "Pedro") {
+    console.log("O nome dele é Pedro");
+} else if (nome == "João") {
+    console.log("O seu nome é João") 
+    //else if pode ser adicionado quantas vezes quiser.
+} else {
+    console.log("Ele possui outro nome. O nome dele é " + nome)
+}
+
+var idade = 21;
+
+if(idade >= 20) {
+    console.log("Ele pode entrar na festa!")
+} else if (idade >= 18) {
+    console.log("Ele só pode entrar com autorização")
+}
