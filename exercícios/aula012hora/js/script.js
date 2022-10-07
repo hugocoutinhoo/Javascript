5e2fb7e399ab2246c6a6eaed67ca603142ddea51
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-10-07 17:22:04 -0300

    aula012 operadores de comparação

---
+++ exercícios/aula012hora/js/script.js
@@ -0,0 +1,24 @@
+var idade = 17;
+var possuiCarro = 0; //o 0 é dado como false no JS
+
+if (idade >= 18) {
+    console.log("usuário pode tirar a habilitação")
+}
+
+if (idade <= 17) {
+    console.log("o usuário não pode fazer a carteira")
+}
+
+if(possuiCarro) {
+    console.log("O usuário pode andar de carro")
+}
+
+var nome = "João";
+
+if(nome == "Hugo") {
+    console.log("O seu nome é Hugo");
+}
+
+if(nome != "Hugo") {
+    console.log("o seu nome não é Hugo")
+}
