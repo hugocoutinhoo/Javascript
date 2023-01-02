9902d4a3dcc308d7645dc7934df6361abbd635ac
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-01-02 17:01:01 -0300

    exercício de soma js e html

---
+++ modulo_c/ex001_eventos-DOM/script.js
@@ -0,0 +1,9 @@
+function somar() {
+    let tn1 = document.getElementById('txtn1');
+    let tn2 = document.getElementById('txtn2');
+    let res = document.getElementById('res');
+    let n1 = Number(tn1.value);
+    let n2 = Number(tn2.value);
+    let s = n1 + n2;
+    res.innerHTML = `A soma entre ${n1} e ${n2} é: ${s}.`;
+}
