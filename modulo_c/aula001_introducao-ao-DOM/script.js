c42d51750cf604fd74eb5644112095d1bcb6a014
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-01-02 16:31:48 -0300

    introdução ao DOM

---
+++ modulo_c/aula001_introducao-ao-DOM/script.js
@@ -0,0 +1,7 @@
+var p1 = window.document.getElementsByTagName('p')[1]
+var corpo = window.document.body;
+/* corpo.style.background = 'darkgray'
+p1.style.color = 'blue'; */
+
+var h1 = window.document.querySelector('h1');
+h1.style.fontFamily = 'Genebra'
