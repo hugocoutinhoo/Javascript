32750340c0ba2cd6d05441345ed6476f8b0bfd2f
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-01-03 10:17:56 -0300

    repetições

---
+++ modulo_e/aula001_repeticoes-pt1/script.js
@@ -0,0 +1,19 @@
+//---------Utilizando while
+/* var fatiaPizza = 1
+while (fatiaPizza <= 8) {
+    console.log(`Comi a fatia ${fatiaPizza}`)
+    fatiaPizza++
+} */
+
+//-------------Utilizando do while
+/* let fatiaPizza = 1
+do {
+    console.log(`Comi a fatia ${fatiaPizza}`);
+    fatiaPizza++
+} while (fatiaPizza <= 8);  */
+
+// Utilizando for
+
+for (let fatiaPizza = 1; fatiaPizza <= 8; fatiaPizza ++) {
+    console.log(`Eu comi a fatia ${fatiaPizza}`);
+}
